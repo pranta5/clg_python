7030d408a2b17e28f27056da656f8049b6efd286
Pranta Das <122752537+pranta5@users.noreply.github.com>
2023-03-20 23:25:48 +0530

    Add files via upload

---
+++ web_scrape_ass1b.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "C:\mca_4th_sem\MoHFW _ Home.html"
+
+soup = BeautifulSoup(open(url,mode='r',encoding='utf-8'),"html.parser")
+
+table = soup.find_all('table')[0]
+
+headers = []
+for th in table.find_all('th'):
+    headers.append(th.text.strip())
+
+rows = []
+for tr in table.tbody.find_all('tr'):
+    row = []
+    for td in tr.find_all('td'):
+        row.append(td.text.strip())
+    rows.append(row)
+
+for row in rows:
+    print(row)
